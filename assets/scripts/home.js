95c468ea0dc85f07bf5f9ed0d308e6f4f04d71ae
Lightuky <47103897+Lightuky@users.noreply.github.com>
2022-02-23 08:05:34 +0100

    add fonts / svg

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -10,9 +10,22 @@ let swiper = new Swiper(".mySwiper", {
         el: ".swiper-pagination",
         clickable: true,
         renderBullet: function (index, className) {
-            return '<div class="m-2 my-3 ' + className + '">' +
+            return '<div class="mx-auto ' + className + '">' +
                 '<div class="sidebarIcon" style="-webkit-mask: url(./build/images/' + icons[index] + '.svg) no-repeat center / contain; mask: url(./build/images/' + icons[index] + '.svg) no-repeat center / contain;"></div>' +
                 '</div>';
         },
     },
 });
+
+
+let swiperPagination = $(".swiper-pagination");
+let navbarHeight = $("#navbar").outerHeight(true);
+let socialIconsHeight = $("#socialIcons").outerHeight(true);
+swiperPagination.css({
+    "height": "calc(100% - " + navbarHeight + "px)",
+    "padding-bottom": Math.round(socialIconsHeight) + "px"
+});
+
+
+
+
