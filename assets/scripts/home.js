e0b3a20f13d20cc029ef584fc879d73e4090153a
Lightuky <47103897+Lightuky@users.noreply.github.com>
2022-03-09 09:25:35 +0100

    home layout/ pagination

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,6 +1,17 @@
 import {Swiper} from "swiper";
 
-let icons = ["hand", "network", "cutlery", "meal", "chief", "calendar", "orders", "tickets", "tick"];
+let icons = [
+    ["hand", "Hello"],
+    ["network", "Présentation"],
+    ["cutlery", "Choix du type de cuisine"],
+    ["meal", "Choix de la recette / Box"],
+    ["chief", "Trouve ton chef"],
+    ["calendar", "Date et adresse de livraison et du RDV"],
+    ["orders", "Récapitulatif"],
+    ["tickets", "Mode de paiement"],
+    ["tick", "Confirmation"],
+];
+let socialIcons = ["facebook", "instagram", "linkedin", "youtube"];
 
 let swiper = new Swiper(".mySwiper", {
     direction: "vertical",
@@ -11,21 +22,29 @@ let swiper = new Swiper(".mySwiper", {
         clickable: true,
         renderBullet: function (index, className) {
             return '<div class="mx-auto ' + className + '">' +
-                '<div class="sidebarIcon" style="-webkit-mask: url(./build/images/' + icons[index] + '.svg) no-repeat center / contain; mask: url(./build/images/' + icons[index] + '.svg) no-repeat center / contain;"></div>' +
+                '<div class="sidebarIcon" title="' + icons[index][1] + '" style="-webkit-mask: url(./build/images/' + icons[index][0] + '.svg) no-repeat center / contain; mask: url(./build/images/' + icons[index][0] + '.svg) no-repeat center / contain;"></div>' +
                 '</div>';
         },
     },
 });
 
 
-let swiperPagination = $(".swiper-pagination");
 let navbarHeight = $("#navbar").outerHeight(true);
 let socialIconsHeight = $("#socialIcons").outerHeight(true);
-swiperPagination.css({
+
+$(".swiper-pagination, .swpSlideContainer").css({
     "height": "calc(100% - " + navbarHeight + "px)",
     "padding-bottom": Math.round(socialIconsHeight) + "px"
 });
 
+$(".btnBookChief").click(function () {
+    $(".swiper-pagination").fadeTo(200, 1);
+    swiper.slideTo(1, 350);
+});
+
+swiper.on('slideChange', function () {
+    $(".swiper-pagination").fadeTo(200, 1);
+});
 
 
 
