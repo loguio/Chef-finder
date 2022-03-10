bb361b831aa5688758bb4513c38e20bc00982de1
Lightuky <47103897+Lightuky@users.noreply.github.com>
2022-03-10 08:05:09 +0100

    add greetings / cooking types slides

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -31,15 +31,35 @@ let swiper = new Swiper(".mySwiper", {
 
 let navbarHeight = $("#navbar").outerHeight(true);
 let socialIconsHeight = $("#socialIcons").outerHeight(true);
+let swpPagination = $(".swiper-pagination").outerWidth(true);
+
 
 $(".swiper-pagination, .swpSlideContainer").css({
     "height": "calc(100% - " + navbarHeight + "px)",
     "padding-bottom": Math.round(socialIconsHeight) + "px"
 });
 
+
+$("#swpCookingTypesContainer").css({
+    "width": "calc(94% - " + swpPagination + "px)"
+});
+
+
+$(".cakeFountain").css({
+    "margin-right": swpPagination + "px",
+    "margin-bottom": socialIconsHeight + "px"
+});
+
+
 $(".btnBookChief").click(function () {
     $(".swiper-pagination").fadeTo(200, 1);
     swiper.slideTo(1, 350);
+});
+
+$(".choiceCard").click(function () {
+    let activeRadio = $(this).find(".choiceRadio");
+    $(".choiceCard, .choiceRadio").removeClass("active");
+    $(this, activeRadio).addClass("active");
 });
 
 swiper.on('slideChange', function () {
