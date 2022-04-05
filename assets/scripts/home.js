3a87642f06c812c8f3ae73d631a64fd6f96c6ff7
Lightuky <47103897+Lightuky@users.noreply.github.com>
2022-04-05 15:50:49 +0200

    cooking types pages

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -13,6 +13,8 @@ let icons = [
 ];
 let socialIcons = ["facebook", "instagram", "linkedin", "youtube"];
 
+
+// Declare the slider component with options and generate the slide buttons based on the icons array
 let swiper = new Swiper(".mySwiper", {
     direction: "vertical",
     slidesPerView: 1,
@@ -30,6 +32,7 @@ let swiper = new Swiper(".mySwiper", {
 });
 
 
+// Manage the containers/pages sizes by calculating the navbar and social icons sizes
 let navbarHeight = $("#navbar").outerHeight(true);
 let socialIconsHeight = $("#socialIcons").outerHeight(true);
 let swpPagination = $(".swiper-pagination").outerWidth(true);
@@ -42,34 +45,65 @@ $(".swiper-pagination, .swpSlideContainer, .formContainer").css({
 
 $(".formContainer").css({"margin-top": navbarHeight + "px"});
 
-$("#swpCookingTypesContainer").css({
+$("#swpCookingTypesContainer, #swpPresentationContainer").css({
     "width": "calc(94% - " + swpPagination + "px)"
 });
 
 
-$(".cakeFountain").css({
+// Position the absolute images by considering the sidebar and social icons sizes
+$(".cakeFountain, .womanChiefImg").css({
     "margin-right": swpPagination + "px",
     "margin-bottom": socialIconsHeight + "px"
 });
 
 
-$(".btnBookChief").click(function () {
+// Get the slide to go to (from buttons), and change the current slide to the desired one with a fade animation
+$(".mainBtn[data-slide-to]").click(function () {
+    let goToSlide = $(this).data("slide-to");
     $(".swiper-pagination").fadeTo(200, 1);
-    swiper.slideTo(1, 350);
+    swiper.slideTo(goToSlide, 350);
 });
 
+
+// Manage the switch of steps for multi-content screens on the homepage
+$(".mainBtn.switchStep").click(function () {
+    $(".firstStep, .secondStep").toggleClass('d-none').css({"animation" : 'fadeBlock 350ms ease-in-out both'});
+});
+
+
+// Toggle active state for cards (text, color, and radio buttons)
 $(".choiceCard").click(function () {
-    let activeRadio = $(this).find(".choiceRadio");
-    $(".choiceCard, .choiceRadio").removeClass("active");
+    $(this).closest('.choiceCardsList').find('.choiceCard').removeClass("active");
     $(this).addClass("active");
-    $(activeRadio).addClass("active");
 });
 
+
+// Toggle active state for list items
+$(".listItem").click(function () {
+    $(this).closest('.overflowedList').find('.listItem').removeClass("active");
+    $(this).addClass("active");
+});
+
+
+// Add a fade animation on slide change
 swiper.on('slideChange', function () {
     $(".swiper-pagination").fadeTo(200, 1);
 });
 
 
+// Tweak the default scroll-wheel behaviour on overflowed list (to avoid conflict with the mousewheel control from SwiperJS)
+let overflowList = $(".overflowedList");
+$(overflowList).mouseenter(function () {
+    swiper.mousewheel.disable()
+});
+
+// Revert to the default slider scroll behaviour when the user cursor leave the list
+$(overflowList).mouseleave(function () {
+    swiper.mousewheel.enable()
+});
+
+
+// Manage the eye icon in login/register forms (toggle to reveal/hide the pwd)
 let pwdInputEyeWidth = $(".pwdInputEye").outerWidth(true);
 
 $(".pwdInput").css({
